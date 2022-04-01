fe1c0cca116eb1ee3f4ef0a7db1c6a450b4f7e36
smal <6433336+smaldragon@users.noreply.github.com>
2022-04-01 20:21:35 +0100

    initial commit

---
+++ capyasm.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python3
+# __CapyASM__
+#                                 .;o,
+#        __."iIoi,._              ;pI __-"-xx.,_
+#      `.3"P3PPPoie-,.            .d' `;.     `p;
+#     `O"dP"````""`PdEe._       .;'   .     `  `|  
+#    "$#"'            ``"P4rdddsP'  .F.    ` `` ; 
+#   i/"""     *"Sp.               .dPff.  _.,;Gw'
+#   ;l"'     "  `dp..            "sWf;fe|'
+#  `l;          .rPi .    . "" "dW;;doe;
+#   $          .;PE`'       " "sW;.d.d;
+#   $$        .$"`     `"saed;lW;.d.d.i
+#   .$M       ;              ``  ' ld;.p.
+#__ _`$o,.-__  "ei-Mu~,.__ ___ `_-dee3'o-ii~m. ____
+
+import sys
+import getopt
+import syntax
+import re
+
+def value_parse(symbol):
+    if len(symbol)==0:
+        return [0]
+    # decimal
+    if symbol[0] == "#":    
+        return [int(symbol[1:])]
+    # hexadeximal
+    if symbol[0] == "$":    
+        return [int(symbol[1:],16)]
+    # binary
+    if symbol[0] == "%":  
+        return [int(symbol[1:],2)]
+    # ascii
+    if len(symbol)>=2:
+        if symbol[0] == '"' and symbol[-1] == '"':
+            out = []
+            for char in symbol[1:-1]:
+                out.append(ord(char))
+            return out
+    
+    try:
+        return [int(symbol)]
+    except:
+        return [symbol]
+symbol_split = re.compile(".*,?")
+value_get = re.compile('".+"|\$*%*@*[0-9a-zA-Z]+')
+modes=[]
+for regex in syntax.addr:
+    modes.append((re.compile(regex),syntax.addr[regex]))
+
+def main(argv):
+    in_file = ''
+    out_file = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('capyasm.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            in_file = arg
+        elif opt in ("-o", "--ofile"):
+            out_file = arg
+    run(in_file,out_file)
+
+def run(in_file,out_file):
+    pc  = 0
+    variables = {}
+    labels = {}
+    cur_label = []
+    out = []
+
+    with open(in_file) as f:
+        lines = f.readlines()
+        i = 0
+        while i < len(lines):
+            line=lines[i]
+            # Remove comments
+            if ";" in line:
+                line = line[:line.find(";")]
+            # Get Tokens for this line
+            tokens = re.findall(r'".+"|[^,\s]+', line.strip())
+            print(tokens)
+            if len(tokens)>=1:
+                if len(tokens[0]) == 0:
+                    continue
+                # Labels
+                if tokens[0].startswith('_'):
+                    indent = 0
+                    label = tokens[0][1:]
+                    for c in label:
+                        if c == '_':
+                            indent+=1
+                    if indent <= len(cur_label):
+                        cur_label = cur_label[:indent] + [label.strip('_')]
+                    print(cur_label)
+                    labels["_".join(cur_label)]=pc
+                elif tokens[0] in syntax.opcodes:
+                    opcode = tokens[0]
+                    addr = "i"
+                    to_append = []
+                    values = []
+                    if len(tokens)>=1:
+                        for operand in tokens[1:]:
+                            for regex in modes:
+                                if regex[0].match(operand):
+                                    addr = regex[1]
+                            values.append(value_get.search(operand).group())
+                        if addr in syntax.opcodes[opcode]:
+                            for symbol in syntax.opcodes[opcode][addr]:
+                                if type(symbol) is int:
+                                    to_append.append(symbol)
+                                else:
+                                    if symbol == "#":
+                                        to_append.extend(value_parse(values[0]))
+                                    elif symbol == "al":
+                                        value = value_parse(values[0])[0]
+                                        if type(value) is int:
+                                            to_append.append(value & 255)
+                                        else:
+                                            label = '_'.join(cur_label[:-1]) + "_" + values[0]
+                                            to_append.append(label)
+                                    elif symbol == "ah":
+                                        value = value_parse(values[0])[0]
+                                        if type(value) is int:
+                                            to_append.append(value >> 8)
+                                        else:
+                                            to_append.append("&high")
+                                    elif symbol == "z":
+                                        to_append.extend(value_parse(values[0]))
+                                    elif symbol == "*":
+                                        for value in values:
+                                            to_append.extend(value_parse(value))
+                                    elif symbol == "**":
+                                        for value in values:
+                                            v = value_parse(value)
+                                            if type(v[0]) is int:
+                                                to_append.append(value_parse(value)[0]&255)
+                                                to_append.append(value_parse(value)[0]>>8)
+                                            else:
+                                                to_append.append(v[0])
+                                                to_append.append("&high")
+                                    elif symbol == "r":
+                                        label = '_'.join(cur_label[:-1]) + "_" + values[0]
+                                        to_append.append([label,pc+2])
+                            if opcode == "org":
+                                pc = value_parse(values[0])[0]
+                            if opcode == "pad":
+                                if addr == "a":
+                                    to_append = [0]*(value_parse(values[0])[0]-pc)
+                                    print("padded",(value_parse(values[0])[0]-pc),"bytes")
+                                if addr == "#":
+                                    to_append = [0]*(value_parse(values[0])[0])                
+                                    print("padded",(value_parse(values[0])[0]),"bytes")
+                            if opcode == "var":
+                                variables[values[0]]=value_parse(values[1])[0]
+                            if opcode == "asm":
+                                with open(values[0].strip('"'),"r") as f:
+                                    lines = lines[:i+1] + f.readlines() + lines[i+1:]
+                            if opcode == "bin":
+                                with open(values[0].strip('"'),"rb") as f:
+                                    bindata = f.read()
+                                    out.extend(bindata)
+                        else:
+                            print(f"ERROR: Opcode '{opcode}' does not have addressing mode '{regex[1]}'")
+                        
+                        out.extend(to_append)
+                    pc += len(to_append)
+                else:
+                    print("ERROR: INVALID TOKEN",tokens[0])
+            i+=1
+
+    # 2nd Pass -> Add labels and variables
+    print(labels,variables,out)
+    for i,s in enumerate(out):
+        if type(s) is str:
+            value = 0
+            if s in labels:
+                value = labels[s]
+            if s in variables:
+                value = variables[s]
+            
+            out[i] = value&255
+            if i+1 < len(out) and out[i+1] == "&high":
+                out[i+1] = value>>8            
+        elif type(s) is list:
+            out[i]   = labels[s[0]]-s[1]
+            if out[i] < 0:
+                out[i] = 256+out[i]
+    with open(out_file,"wb") as f:
+        f.write(bytearray(out))
+    
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ readme.md
@@ -0,0 +1,33 @@
+# CapyASM
+
+**CapyASM** is a 65c02 assembler built as a learning exercise. Its syntax, particularly in regards to addressing modes, differs from the typical 65xx syntax, with the goal of trying to make the instructions more explicit and easy to understand.
+
+> Note: This assembler is very much a WIP, I do not recommend using it in projects.
+
+## Usage
+
+`python3 capyasm.py -i input.asm -o output.asm`
+
+## Addressing Modes
+
+* **Implied** - `RTS`
+* **Registers** - `PSH A | PSH X | PSH Y | PSH P`
+* **Immediate** - `LDA $20`
+* **Relative** - `BRA (label)`
+* **Absolute** - `LDA [$2000]`
+* **Zero Page** - `LDA <$20>`
+* **Indirect** - `JMP [[$1000]]`
+* **Absolute Indexed** - `LDA [$1000+x]`
+* **Zero Page Indexed** - `LDA <$10+x>`
+* **Indexed Indirect** - `LDA [<$10+x>]`
+* **Indirect Indexed** - `LDA [<$10>+y]`
+```
+
+## Assembler Commands
+
+* `byte $xx,$xx,(...)` - Inserts binary byte data
+* `word $xxxx,$xxxx`   - Inserts binary word data
+* `bin "file.bin"`     - Inserts a binary file
+* `asm "file.asm"`     - Inserts an assembly file
+* `org [$xx]`          - Sets the Program Counter
+* `pad $xx|pad $[xx]`  - Adds zeros to the file, either a set amount or until a certain PC is reached
